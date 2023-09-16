43432d53497c8c74b5843f182917ac988550d1dc
Jerson Suaza <jerson@netuxtecnologia.com>
2023-09-16 10:34:04 -0500

    topbar nativation

--- quehaypahacer-app/src/components/Topbar/index.jsx
+++ quehaypahacer-app/src/components/Topbar/index.jsx
@@ -1,10 +1,28 @@
-import { TopbarContainer } from "./styles"
+import { TopbarContainer, TopbarIcon, TopbarTitle } from "./styles"
+import { IoHomeOutline, IoPersonCircleOutline, IoLogInOutline } from 'react-icons/io5'
+import { Link } from 'react-router-dom'
 
 export const Topbar = () => {
 
+  const isAuth = true
+
   return (
     <TopbarContainer>
-      <h5>Que hay pa' hacer</h5>
+      <TopbarIcon>
+        <Link to={'/'}>
+          <IoHomeOutline />
+        </Link>
+      </TopbarIcon>
+      <TopbarTitle>
+        <h5>Que hay pa' hacer</h5>
+      </TopbarTitle>
+      <TopbarIcon>
+        {
+          isAuth
+            ? <Link to={'/profile'}><IoPersonCircleOutline /></Link>
+            : <Link to={'/login'}><IoLogInOutline /></Link>
+        }
+      </TopbarIcon>
     </TopbarContainer>
   )
 }
--- quehaypahacer-app/src/components/Topbar/styles.jsx
+++ quehaypahacer-app/src/components/Topbar/styles.jsx
@@ -8,6 +8,7 @@ export const TopbarContainer = styled.header`
   top: 0;
   left: 0;
   background-color: ${COLORS.primary};
+  display: flex;
 
   h5 {
     color: #fff;
@@ -15,4 +16,29 @@ export const TopbarContainer = styled.header`
     font-size: 1.2em;
     line-height: 0;
   }
+
+`
+
+export const TopbarIcon = styled.div`
+  width: 60px;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  svg {
+    color: #f2f2f2;
+    font-size: 1.6em;
+    opacity: 0.8;
+  }
+
+  &:hover {
+    svg {
+      opacity: 1;
+    }
+  }
+`
+
+export const TopbarTitle = styled.div`
+  flex: 1;
 `
